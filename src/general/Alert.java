179f3688668922e27ba49e3890a1e5ee25587450
Zachery <zacheryschiller@gmail.com>
2014-03-24 18:56:55 -0400

    Updated to output "Out of bounds!" to console + fixed naming to match design and types to float.

--- src/general/Alert.java
+++ src/general/Alert.java
@@ -2,15 +2,23 @@ package general;
 
 public class Alert {
 
-	//If current Latitude or Longitude is outside of the boundary, return True.
-	public static Boolean boundaryAlert(double latCurrent, double longCurrent) {
-		if (latCurrent > Settings.getLatMax() || 
-				latCurrent < Settings.getLatMin() || 
-				longCurrent > Settings.getLongMax()
-				|| longCurrent < Settings.getLongMin()) {
+	// If current Latitude or Longitude is outside of the boundary, return True.
+	// Need to update so that it displays which direction is out of bounds.
+	public static Boolean outsideBoundary(float latCurrent, float lonCurrent) {
+		if (latCurrent > Settings.getLatMax()
+				|| latCurrent < Settings.getLatMin()
+				|| lonCurrent > Settings.getLonMax()
+				|| lonCurrent < Settings.getLonMin()) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+
+	public static void boundaryAlert() {
+		if (outsideBoundary(GPS.getLat(), GPS.getLon())) {
+			System.out.println("Alpaca out of bounds!");
+		}
+	}
+
 }
