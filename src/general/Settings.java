16dbac9a522ab016f6fd2d7b6e393256a36379a3
Zachery <zacheryschiller@gmail.com>
2014-03-24 19:26:15 -0400

    Started file IO stuff

--- src/general/Settings.java
+++ src/general/Settings.java
@@ -1,7 +1,39 @@
 package general;
 
-public class Settings {
+import java.io.*;
 
+public class Settings {
+	/**
+	 * I/O code copied from www.caveofprogramming.com: http://goo.gl/LV2hYk
+	 */
+	public static void main(String[] args) {
+		// The name of the file to open.
+		String fileName = "boundaries.txt";
+
+		// This will reference one line at a time
+		String line = null;
+
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = new FileReader(fileName);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			while ((line = bufferedReader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			// Always close files.
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fileName + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + fileName + "'");
+		}
+	}
+
+	/////
 	// Boundaries in Latitude and longitude Maximum and Minimum
 	static float latMax, latMin, lonMax, lonMin;
 
