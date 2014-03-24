d17118ac3bdbaf99ca13dedeea4f8936f73e00cd
Zachery <zacheryschiller@gmail.com>
2014-03-24 18:57:15 -0400

    Fixed naming to match design and types to float.

--- src/general/Settings.java
+++ src/general/Settings.java
@@ -1,38 +1,39 @@
 package general;
 
 public class Settings {
-	//Boundaries in Latitude and Longitude Maximum and Minimum
-	static double latMax, latMin, longMax, longMin;
 
-	public static double getLatMax() {
+	// Boundaries in Latitude and longitude Maximum and Minimum
+	static float latMax, latMin, lonMax, lonMin;
+
+	public static float getLatMax() {
 		return latMax;
 	}
 
-	public static void setLatMax(double latMax) {
+	public static void setLatMax(float latMax) {
 		Settings.latMax = latMax;
 	}
 
-	public static double getLatMin() {
+	public static float getLatMin() {
 		return latMin;
 	}
 
-	public static void setLatMin(double latMin) {
+	public static void setLatMin(float latMin) {
 		Settings.latMin = latMin;
 	}
 
-	public static double getLongMax() {
-		return longMax;
+	public static float getLonMax() {
+		return lonMax;
 	}
 
-	public static void setLongMax(double longMax) {
-		Settings.longMax = longMax;
+	public static void setLonMax(float lonMax) {
+		Settings.lonMax = lonMax;
 	}
 
-	public static double getLongMin() {
-		return longMin;
+	public static float getLonMin() {
+		return lonMin;
 	}
 
-	public static void setLongMin(double longMin) {
-		Settings.longMin = longMin;
+	public static void setLonMin(float lonMin) {
+		Settings.lonMin = lonMin;
 	}
 }
