9d9cb98a6faf4bcbe62eb2716f1b32365d2683eb
Zachery <zacheryschiller@gmail.com>
2014-03-24 22:09:42 -0400

    Added get and set for user interaction and storing to bounaries file.

--- src/general/Settings.java
+++ src/general/Settings.java
@@ -3,28 +3,32 @@ package general;
 import java.io.*;
 
 public class Settings {
-	/**
-	 * I/O code copied from www.caveofprogramming.com: http://goo.gl/LV2hYk
-	 */
-	public static void main(String[] args) {
-		// The name of the file to open.
+
+	// Boundaries in Latitude and longitude Maximum and Minimum
+	static float latMax, latMin, lonMax, lonMin;
+
+	// Get the boundaries from the boundaries.txt file
+	public static void getBoundaries() {
+		// The name of the file that the boundaries are kept in
 		String fileName = "boundaries.txt";
 
-		// This will reference one line at a time
 		String line = null;
-
 		try {
-			// FileReader reads text files in the default encoding.
 			FileReader fileReader = new FileReader(fileName);
-
-			// Always wrap FileReader in BufferedReader.
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-
+			// Read in boundaries and set them
 			while ((line = bufferedReader.readLine()) != null) {
-				System.out.println(line);
+				String[] data = line.split("[:]");
+				if (data[0].equals("latMax")) {
+					setLatMax(Float.valueOf(data[1]));
+				} else if (data[0].equals("latMin")) {
+					setLatMin(Float.valueOf(data[1]));
+				} else if (data[0].equals("lonMax")) {
+					setLonMax(Float.valueOf(data[1]));
+				} else if (data[0].equals("lonMin")) {
+					setLonMin(Float.valueOf(data[1]));
+				}
 			}
-
-			// Always close files.
 			bufferedReader.close();
 		} catch (FileNotFoundException ex) {
 			System.out.println("Unable to open file '" + fileName + "'");
@@ -33,9 +37,41 @@ public class Settings {
 		}
 	}
 
-	/////
-	// Boundaries in Latitude and longitude Maximum and Minimum
-	static float latMax, latMin, lonMax, lonMin;
+	// Set the boundaries from user input and save to file
+	public static void setBoundaries() throws IOException {
+		// Get input from user for max and min lat and lon
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(System.in));
+		System.out.println("Please enter the max latitude: ");
+		String s = bufferedReader.readLine();
+		setLatMax(Float.valueOf(s));
+		System.out.println("Please enter the min latitude: ");
+		s = bufferedReader.readLine();
+		setLatMin(Float.valueOf(s));
+		System.out.println("Please enter the max longitude: ");
+		s = bufferedReader.readLine();
+		setLonMax(Float.valueOf(s));
+		System.out.println("Please enter the min longitude: ");
+		s = bufferedReader.readLine();
+		setLonMin(Float.valueOf(s));
+
+		// Store the max and min lat and lon into boundaries.txt
+		String fileName = "boundaries.txt";
+		try {
+			FileWriter fileWriter = new FileWriter(fileName);
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.write("latMax:" + getLatMax());
+			bufferedWriter.newLine();
+			bufferedWriter.write("latMin:" + getLatMin());
+			bufferedWriter.newLine();
+			bufferedWriter.write("lonMax:" + getLonMax());
+			bufferedWriter.newLine();
+			bufferedWriter.write("lonMin:" + getLonMin());
+			bufferedWriter.close();
+		} catch (IOException ex) {
+			System.out.println("Error writing to file '" + fileName + "'");
+		}
+	}
 
 	public static float getLatMax() {
 		return latMax;
