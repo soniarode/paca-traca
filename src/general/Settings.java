d79853b5ba1233c87759aedccc6b766ee85586ce
Zachery <zacheryschiller@gmail.com>
2014-03-27 19:17:55 -0400

    Fixed my own problem.  Polease enter the commit message for your changes. Lines starting

---
+++ src/general/Settings.java
@@ -0,0 +1,22 @@
+package general;
+
+public interface Settings {
+
+	public void getBoundaries();
+	public void setBoundaries();
+	public float getLatMax();
+
+	public void setLatMax(float latMax);
+
+	public float getLatMin();
+
+	public void setLatMin(float latMin);
+
+	public float getLonMax();
+
+	public void setLonMax(float lonMax);
+
+	public float getLonMin();
+
+	public void setLonMin(float lonMin);
+}
